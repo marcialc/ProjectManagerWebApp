0239d6e9a986cd25a8f188f9b4ea486b280995af
Tyler Garcia <tyleraus96@gmail.com>
2020-04-30 21:11:48 -0400

    home grid

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import '../css/Home.css'
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Tyler</h1>
-      </div>
+      <div class="grid-container">
+  <div class="story-feed"><h1>yo</h1></div>
+  <div class="calendar"><h1>yo</h1></div>
+  <div class="header-toolbar"><h1>yo</h1></div>
+</div>
     );
   }
 }
